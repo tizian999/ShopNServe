2501d0b032b8e661ebd7e6a3617a48880b761722
tizianwalter <tizian.walter@student.hs-rm.de>
2026-03-11 16:31:07 +0100

    Deleted some irrelevant files

--- Frontend/shell/src/ReactMicroclientWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MicroclientApp from '../../microclient-react/src/App';
-
-const ReactMicroclientWrapper = () => {
-    return <MicroclientApp />;
-};
-
-export default ReactMicroclientWrapper;
